b8beca2d3314bc070230d04ddc854508dd71020a
Viktoria <michinyan1404@gmail.com>
2023-07-26 22:32:00 +0300

    Написана программа

---
+++ itog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+ 
+using static System.Console;
+ 
+Clear();
+ 
+Write("Введите кол-во элементов массива: ");
+int n = int.Parse(ReadLine());
+string[] array = new string[n];
+array = GetArray(n);
+
+string[] GetArray(int n)
+{
+    for (int i = 0; i<n; i++)
+    {
+        Write($"Введите {i+1}-й элемент массива: ");
+        array[i] = ReadLine();
+    }
+    return array;
+}
+
+for (int i = 0; i<n; i++)
+{
+    if (array[i].Length > 2)
+    {
+        WriteLine($"{array[i]}");
+    }
+}
